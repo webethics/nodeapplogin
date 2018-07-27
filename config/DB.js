e2894709c6735b66ddeeb5c9c2fa53bb5239021c
webethics <contact.webeethics@gmail.com>
2018-07-27 15:22:59 +0530

    first commit

---
+++ config/DB.js
@@ -0,0 +1,4 @@
+module.exports = {
+   'secret': 'appsecret',
+   DB: 'mongodb://muser:kamal123@ds129821.mlab.com:29821/mongo_db'
+};
---
+++ config/constant.js
@@ -0,0 +1,6 @@
+module.exports = {
+   upload_dir: 'public/uploads',
+    //front_end_url: 'http://localhost:4200',
+   front_end_url: 'https://nodewebethics.herokuapp.com',
+   EmailTamplatePath:'public/pages'
+};
